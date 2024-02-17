bfdd380ffcdbc66b0dabeaf8760e9c5274f5b587
Hasan <thehasandev@gmail.com>
2024-02-17 19:25:11 +0600

    add flex component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Container from './components/Container'
+import Flex from './components/Flex'
 
 function App() {
   return (
     <div>
       <Container>
-        <h1 className='text-3xl font-roboto'>Lorem ipsum dolor sit amet.</h1>
-        <h1 className='text-3xl font-pop'>Lorem ipsum dolor sit amet.</h1>
-
+       <Flex className="justify-between">
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+       </Flex>
       </Container>
     </div>
   )
