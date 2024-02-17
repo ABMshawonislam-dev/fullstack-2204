450475b33ff88263aab210ab306b63a498dac125
Hasan <thehasandev@gmail.com>
2024-02-17 19:11:20 +0600

    add container component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Container from './components/Container'
 
 function App() {
   return (
     <div>
-      <h1>Tailwind setup</h1>
+      <Container>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore expedita quod architecto dignissimos vel. Eveniet numquam commodi ipsam enim, asperiores, voluptatum cum modi sequi quae cumque voluptas, nam maiores. Est officia, dolore sit eum, accusamus voluptatum corporis in error laborum quo sequi alias saepe, neque iusto repudiandae voluptatibus pariatur facere iure magnam illo voluptatem. Aliquam doloremque reprehenderit voluptate accusantium, accusamus sapiente repudiandae, totam nemo neque repellendus harum. Tenetur, nemo quisquam molestias neque minima delectus aliquid inventore consequatur ratione eveniet accusantium culpa optio distinctio ullam fuga suscipit alias cum, cumque deserunt iusto aliquam nisi dicta. Aspernatur unde beatae cupiditate autem nostrum!
+      </Container>
     </div>
   )
 }
