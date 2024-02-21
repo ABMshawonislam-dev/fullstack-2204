e05270fe2d763c41ec70b067f4b06812c7dd0fe8
MD IBN ALAM <97562405+ibnalam@users.noreply.github.com>
2024-02-21 17:24:00 +0600

    banner section done

--- frontend/star-tach/src/pages/Home.jsx
+++ frontend/star-tach/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Topnav from "../components/layout/Topnav.jsx"
 import Catagorynav from "../components/layout/Catagorynav.jsx"
+import Banner from '../components/layout/Banner.jsx'
 
 const Home = () => {
   return (
     <>
     <Topnav/>
-    <Catagorynav/>
+    <Catagorynav />
+    <Banner/>
     </>
   )
 }
