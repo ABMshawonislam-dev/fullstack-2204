1d30d536af2724e74361f35fad5bd878b21f34fd
MD IBN ALAM <97562405+ibnalam@users.noreply.github.com>
2024-02-21 18:26:06 +0600

    bannarfooter

--- frontend/star-tach/src/pages/Home.jsx
+++ frontend/star-tach/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Topnav from "../components/layout/Topnav.jsx"
 import Catagorynav from "../components/layout/Catagorynav.jsx"
 import Banner from '../components/layout/Banner.jsx'
+import Bannerfooter from '../components/layout/Bannerfooter.jsx'
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
     <Topnav/>
     <Catagorynav />
     <Banner/>
+    <Bannerfooter/>
     </>
   )
 }
