852e6ca9a329faf8a86b4457346ff1b4140da4cc
MD IBN ALAM <97562405+ibnalam@users.noreply.github.com>
2024-02-21 20:27:21 +0600

    Newsfeed

--- frontend/star-tach/src/pages/Home.jsx
+++ frontend/star-tach/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+
+
 import Topnav from "../components/layout/Topnav.jsx"
 import Catagorynav from "../components/layout/Catagorynav.jsx"
 import Banner from '../components/layout/Banner.jsx'
 import Bannerfooter from '../components/layout/Bannerfooter.jsx'
+import NewsFeed from '../components/layout/NewsFeed.jsx'
 
 const Home = () => {
   return (
@@ -11,6 +14,7 @@ const Home = () => {
     <Catagorynav />
     <Banner/>
     <Bannerfooter/>
+    <NewsFeed/>
     </>
   )
 }
