af33273e08ca868de7fbac5bc3238d18b538aa28
MD IBN ALAM <97562405+ibnalam@users.noreply.github.com>
2024-02-22 22:27:32 +0600

    Featured product

--- frontend/star-tach/src/pages/Home.jsx
+++ frontend/star-tach/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Banner from '../components/layout/Banner.jsx'
 import Bannerfooter from '../components/layout/Bannerfooter.jsx'
 import NewsFeed from '../components/layout/NewsFeed.jsx'
 import Cetagory from '../components/layout/Cetagory.jsx'
+import Product from '../components/layout/Product.jsx'
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
     <Bannerfooter/>
     <NewsFeed/>
     <Cetagory/>
+    <Product/>
     </>
   )
 }
